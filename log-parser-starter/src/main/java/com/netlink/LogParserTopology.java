5910c3da5ed289c315935d204e99d088217c6d86
fubencheng <fubencheng310@sina.com>
2018-06-27 22:55:28 +0800

    添加starter

---
+++ log-parser-starter/src/main/java/com/netlink/LogParserTopology.java
@@ -0,0 +1,15 @@
+package com.netlink;
+
+/**
+ * LogParserTopology.
+ *
+ * @author fubencheng.
+ * @version 0.0.1 2018-06-11 23:30 fubencheng.
+ */
+public class LogParserTopology {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+}
